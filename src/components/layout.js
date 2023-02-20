f2751b1567c2e9cf4585420f1d1d4570ed91ee21
Kun <yukun0620@gmail.com>
2023-02-21 00:20:05 +0800

    login page

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,6 +20,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import Grid from "@mui/material/Grid";
+import Button from '@mui/material/Button';
 import PersonIcon from "@mui/icons-material/Person";
 import Avatar from "@mui/material/Avatar";
 import { useTheme } from "@mui/material/styles";
@@ -28,6 +29,8 @@ import Paper from "@mui/material/Paper";
 import MenuList from "@mui/material/MenuList";
 import MenuItem from "@mui/material/MenuItem";
 import Fade from "@mui/material/Fade";
+import TextField from '@mui/material/TextField';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import ClickAwayListener from "@mui/material/ClickAwayListener";
 
 const drawerWidth = 200;
@@ -35,6 +38,7 @@ const drawerWidth = 200;
 export default function Layout(props) {
   const theme = useTheme();
   const { window } = props;
+  const [account, setAccount] = useState("");
   const [mobileOpen, setMobileOpen] = useState(false);
   const [accountMenuOpen, setAccountMenuOpen] = useState(false);
 
@@ -102,123 +106,181 @@ export default function Layout(props) {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar sx={{ position: "relative" }}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            我是標題
-          </Typography>
-          <ClickAwayListener
-            onClickAway={() => {
-              setAccountMenuOpen(false);
+      {account && (
+        <>
+          <AppBar
+            position="fixed"
+            sx={{
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+              ml: { sm: `${drawerWidth}px` },
             }}
           >
-            <Box sx={{ ml: "auto" }}>
-              <Avatar
-                sx={{
-                  width: 28,
-                  height: 28,
-                  bgcolor: "white",
-                  color: theme.palette.primary.main,
-                  cursor: "pointer",
-                  "&:hover": {
-                    bgcolor: grey[300],
-                  },
-                }}
-                onClick={() => {
-                  setAccountMenuOpen(!accountMenuOpen);
+            <Toolbar sx={{ position: "relative" }}>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                我是標題
+              </Typography>
+              <ClickAwayListener
+                onClickAway={() => {
+                  setAccountMenuOpen(false);
                 }}
               >
-                <PersonIcon />
-              </Avatar>
-              <Fade in={accountMenuOpen}>
-                <Paper sx={{ position: "absolute", right: 10, top: 50 }}>
-                  <MenuList>
-                    <MenuItem>
-                      <ListItemText
-                        sx={{ textAlign: "center" }}
-                        primary={"Bruce"}
-                        secondary={"Admin"}
-                      />
-                    </MenuItem>
-                    <Divider variant="middle"></Divider>
-                    <MenuItem
-                      onClick={() => {
-                        setAccountMenuOpen(false);
-                      }}
-                    >
-                      Logout
-                    </MenuItem>
-                  </MenuList>
-                </Paper>
-              </Fade>
+                <Box sx={{ ml: "auto" }}>
+                  <Avatar
+                    sx={{
+                      width: 28,
+                      height: 28,
+                      bgcolor: "white",
+                      color: theme.palette.primary.main,
+                      cursor: "pointer",
+                      "&:hover": {
+                        bgcolor: grey[300],
+                      },
+                    }}
+                    onClick={() => {
+                      setAccountMenuOpen(!accountMenuOpen);
+                    }}
+                  >
+                    <PersonIcon />
+                  </Avatar>
+                  <Fade in={accountMenuOpen}>
+                    <Paper sx={{ position: "absolute", right: 10, top: 50 }}>
+                      <MenuList>
+                        <MenuItem>
+                          <ListItemText
+                            sx={{ textAlign: "center" }}
+                            primary={"Bruce"}
+                            secondary={"Admin"}
+                          />
+                        </MenuItem>
+                        <Divider variant="middle"></Divider>
+                        <MenuItem
+                          onClick={() => {
+                            setAccountMenuOpen(false);
+                          }}
+                        >
+                          Logout
+                        </MenuItem>
+                      </MenuList>
+                    </Paper>
+                  </Fade>
+                </Box>
+              </ClickAwayListener>
+            </Toolbar>
+          </AppBar>
+          <Box
+            component="nav"
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          >
+            <Drawer
+              container={container}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              sx={{
+                display: { xs: "block", sm: "none" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+            >
+              {drawer}
+            </Drawer>
+            <Drawer
+              variant="permanent"
+              sx={{
+                display: { xs: "none", sm: "block" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              p: 3,
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+            }}
+          >
+            <Toolbar />
+            <Grid container justifyContent="center">
+              {props.children}
+            </Grid>
+          </Box>
+        </>
+      )}
+      {!account && (
+        <>
+          <Box
+            sx={{
+              mx: 'auto',
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              阿婆仔菜後台系統
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={()=>{console.log('submit')}}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="account"
+                label="帳號"
+                name="account"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="密碼"
+                type="password"
+                id="password"
+              />
+              <Button
+                onClick={()=>{setAccount('Bruce')}}
+                type="button"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                登入
+              </Button>
             </Box>
-          </ClickAwayListener>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-      >
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Grid container justifyContent="center">
-          {props.children}
-        </Grid>
-      </Box>
+          </Box>
+        </>
+      )}
     </Box>
   );
 }
