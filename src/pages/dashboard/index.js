bca01ec8c9b03b66b0f4ef4b4cf1595127eff77e
Kun <yukun0620@gmail.com>
2023-02-21 01:42:08 +0800

    dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,65 @@
+import { Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
+const rows = [
+  {
+    title: "總訂單數",
+    content: "100",
+    portion: 2
+  },
+  {
+    title: "總營業額",
+    content: "100",
+    portion: 6
+  },
+  {
+    title: "平均客單價",
+    content: "100",
+    portion: 3
+  },
+  {
+    title: "平均月營業額",
+    content: "100",
+    portion: 6
+  },
+  {
+    title: "總顧客數",
+    content: "100",
+    portion: 3
+  },
+]
+
+export function Block({title, content, portion}) {
+  return (
+    <Paper
+      component={Grid}
+      item
+      container
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      xs={portion}
+      sx={{ height: "100px" }}
+    >
+      <Typography variant="h6" color="black">{title}</Typography>
+      <Typography variant="p" color="black">{content}</Typography>
+    </Paper>
+  )
+  
+}
+
 export default function Dashboard() {
   return (
-    <span>dashboard</span>
-  )
-} 
+    <Grid item container xs={10} justifyContent="center" gap={1}>
+      <Grid xs={12} sx={{border: '2px solid blue', height: '400px'}}>
+      </Grid>
+      {rows.map((row) => {
+        return (
+          <Block key={row.title} title={row.title} content={row.content} portion={row.portion}/>
+        )
+        
+      })}
+    </Grid>
+  );
+}
